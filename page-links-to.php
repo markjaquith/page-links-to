c6b60a30b34a28e8fce2182afa766d09e638f912
Mark Jaquith <m@rkj.me>
2005-07-30 13:56:14 +0000

    Handle posts.  Handle redirects.  Filter get_the_permalink.

--- page-links-to.php
+++ page-links-to.php
@@ -3,7 +3,7 @@
 Plugin Name: Page Links To
 Plugin URI: http://txfx.net/code/wordpress/page-links-to/
 Description: Allows you to set a "links_to" meta key with a URI value that will be be used when listing WP pages.  Good for setting up navigational links to non-WP sections of your 
-Version: 1.0
+Version: 1.1
 Author URI: http://txfx.net/
 */
 
@@ -42,7 +42,7 @@ function txfx_get_page_links_to_meta () {
 		$links_to = $wpdb->get_results(
 		"SELECT	post_id, meta_value " .
 		"FROM $wpdb->postmeta, $wpdb->posts " .
-		"WHERE post_id = ID AND meta_key = 'links_to' AND post_status = 'static'");
+		"WHERE post_id = ID AND meta_key = 'links_to' AND (post_status = 'static' OR post_status = 'publish')");
 		} else {
 			return $page_links_to_cache;
 		}
@@ -59,14 +59,45 @@ function txfx_get_page_links_to_meta () {
 		return $page_links_to_cache;
 	}
 
-function txfx_filter_links_to_pages ($link, $page_id) {
+function txfx_filter_links_to_pages ($link, $post) {
 	$page_links_to_cache = txfx_get_page_links_to_meta();
-
-	if ( $page_links_to_cache[$page_id] )
-		$link = $page_links_to_cache[$page_id];
+	
+	// Really strange, but page_link gives us an ID and post_link gives us a post object
+	$id = ($post->ID) ? $post->ID : $post;
+			
+	if ( $page_links_to_cache[$id] )
+		$link = $page_links_to_cache[$id];
 
 	return $link;
 }
 
-add_filter('page_link', 'txfx_filter_links_to_pages', 10, 2);
+function txfx_redirect_links_to_pages () {
+	if ( is_single() || is_page() ) :
+	global $wp_query;
+	
+		$link = get_post_meta($wp_query->post->ID, 'links_to', true);
+	
+		if(!$link) return;
+		
+		$redirect_type = get_post_meta($wp_query->post->ID, 'links_to_type', true);
+		
+		if ($redirect_type && $redirect_type != '302') {
+			// Only supporting 301 and 302 for now.
+			// The others aren't widely supported or needed anyway
+			header("HTTP/1.0 301 Moved Permanently");
+			header("Status: 301 Moved Permanently");
+			header("Location: $link");
+			exit;
+		}
+
+		// If we got this far, it's a 302 redirect
+		header("Status: 302 Moved Temporarily");
+		wp_redirect($link);
+		exit;
+	endif;
+}
+
+add_action('template_redirect', 'txfx_redirect_links_to_pages');
+add_filter('page_link', 'txfx_filter_links_to_pages', 20, 2);
+add_filter('post_link', 'txfx_filter_links_to_pages', 20, 2);
 ?>
