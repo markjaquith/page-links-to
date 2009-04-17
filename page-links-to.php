84731558e1b635e33b0f2061aca1c03476a7234a
Mark Jaquith <m@rkj.me>
2009-04-17 09:44:19 +0000

    Version 1.7 is live!

--- page-links-to.php
+++ page-links-to.php
@@ -3,7 +3,7 @@
 Plugin Name: Page Links To
 Plugin URI: http://txfx.net/code/wordpress/page-links-to/
 Description: Allows you to point WordPress pages or posts to a URL of your choosing.  Good for setting up navigational links to non-WP sections of your site or to off-site resources.
-Version: 1.6
+Version: 1.7
 Author: Mark Jaquith
 Author URI: http://coveredwebservices.com/
 */
@@ -32,7 +32,7 @@ function txfx_get_page_links_to_meta () {
 		$links_to = $wpdb->get_results(
 		"SELECT post_id, meta_value " .
 		"FROM $wpdb->postmeta, $wpdb->posts " .
-		"WHERE post_id = ID AND meta_key = 'links_to' AND (post_status = 'static' OR post_status = 'publish')");
+		"WHERE post_id = ID AND meta_key = '_links_to' AND (post_status = 'static' OR post_status = 'publish')");
 	} else {
 		return $page_links_to_cache;
 	}
@@ -42,9 +42,8 @@ function txfx_get_page_links_to_meta () {
 		return false;
 	}
 
-	foreach ( (array) $links_to as $link ) {
+	foreach ( (array) $links_to as $link )
 		$page_links_to_cache[$link->post_id] = $link->meta_value;
-	}
 
 	return $page_links_to_cache;
 }
@@ -56,7 +55,7 @@ function txfx_get_page_links_to_targets () {
 		$links_to = $wpdb->get_results(
 		"SELECT post_id, meta_value " .
 		"FROM $wpdb->postmeta, $wpdb->posts " .
-		"WHERE post_id = ID AND meta_key = 'links_to_target' AND (post_status = 'static' OR post_status = 'publish')");
+		"WHERE post_id = ID AND meta_key = '_links_to_target' AND (post_status = 'static' OR post_status = 'publish')");
 	} else {
 		return $page_links_to_target_cache;
 	}
@@ -80,19 +79,28 @@ function txfx_plt_add_meta_box( $page, $context ) {
 
 function txfx_plt_meta_box() {
 	global $post;
-
-	if ( get_post_meta( $post->ID, 'links_to', true) )
-		echo '<p>';
-		wp_nonce_field( 'txfx_plt', '_txfx_pl2_nonce', false, true );
-		echo '</p>';
-	?>
-	Point to this URL: <input name="txfx_links_to" type="text" size="65" id="txfx_links_to" value="<?php echo attribute_escape( get_post_meta( $post->ID, 'links_to', true) ); ?>" />
-	<?php
+	echo '<p>';
+	wp_nonce_field( 'txfx_plt', '_txfx_pl2_nonce', false, true );
+	echo '</p>';
+?>
+	<p>Point to this URL: <input name="txfx_links_to" type="text" style="width:75%" id="txfx_links_to" value="<?php echo attribute_escape( get_post_meta( $post->ID, '_links_to', true) ); ?>" /></p>
+	<p><label for="txfx_links_to_new_window"><input type="checkbox" name="txfx_links_to_new_window" id="txfx_links_to_new_window" value="_blank"<?php if ( "_blank" == get_post_meta( $post->ID, '_links_to_target', true ) ) { echo ' checked="checked"'; } ?>> Open this link in a new window</label></p>
+	<p><label for="txfx_links_to_302"><input type="checkbox" name="txfx_links_to_302" id="txfx_links_to_302" value="302"<?php if ( '302' == get_post_meta( $post->ID, '_links_to_type', true ) ) { echo ' checked="checked"'; } ?>> Use a temporary <code>302</code> redirect (default is a permanent <code>301</code> redirect)</label></p>
+<?php
 }
 
 function txfx_plt_save_meta_box( $post_ID ) {
-	if ( wp_verify_nonce( $_REQUEST['_txfx_pl2_nonce'], 'txfx_plt' ) && isset( $_POST['txfx_links_to'] ) && strlen( $_POST['txfx_links_to'] ) > 7 )
-		update_post_meta( $post_ID, 'links_to', $_POST['txfx_links_to'] );
+	if ( wp_verify_nonce( $_REQUEST['_txfx_pl2_nonce'], 'txfx_plt' ) && isset( $_POST['txfx_links_to'] ) && strlen( $_POST['txfx_links_to'] ) > 7 ) {
+		update_post_meta( $post_ID, '_links_to', $_POST['txfx_links_to'] );
+		if ( isset( $_POST['txfx_links_to_new_window'] ) )
+			update_post_meta( $post_ID, '_links_to_target', '_blank' );
+		else
+			delete_post_meta( $post_ID, '_links_to_target' );
+		if ( isset( $_POST['txfx_links_to_302'] ) )
+			update_post_meta( $post_ID, '_links_to_type', '302' );
+		else
+			delete_post_meta( $post_ID, '_links_to_type' );
+	}
 	return $post_ID;
 }
 
@@ -115,12 +123,12 @@ function txfx_redirect_links_to_pages() {
 
 	global $wp_query;
 
-	$link = get_post_meta( $wp_query->post->ID, 'links_to', true );
+	$link = get_post_meta( $wp_query->post->ID, '_links_to', true );
 
 	if ( !$link )
 		return;
 
-	$redirect_type = get_post_meta( $wp_query->post->ID, 'links_to_type', true );
+	$redirect_type = get_post_meta( $wp_query->post->ID, '_links_to_type', true );
 
 	if ( $redirect_type && $redirect_type != '302' ) {
 		// Only supporting 301 and 302 for now.
@@ -171,10 +179,21 @@ function txfx_page_links_to_highlight_tabs( $pages ) {
 	return $pages;
 }
 
+function txfx_plt_init() {
+	if ( get_option( 'txfx_plt_schema_version' ) < 3 ) {
+		global $wpdb;
+		$wpdb->query( "UPDATE $wpdb->postmeta SET meta_key = '_links_to' WHERE meta_key = 'links_to'" );
+		$wpdb->query( "UPDATE $wpdb->postmeta SET meta_key = '_links_to_target' WHERE meta_key = 'links_to_target'" );
+		$wpdb->query( "UPDATE $wpdb->postmeta SET meta_key = '_links_to_type' WHERE meta_key = 'links_to_type'" );
+		wp_cache_flush();
+		update_option( 'txfx_plt_schema_version', 3 );
+	}
+}
+
 add_filter( 'wp_list_pages',     'txfx_page_links_to_highlight_tabs' );
 add_action( 'template_redirect', 'txfx_redirect_links_to_pages'      );
 add_filter( 'page_link',         'txfx_filter_links_to_pages', 20, 2 );
 add_filter( 'post_link',         'txfx_filter_links_to_pages', 20, 2 );
 add_action( 'do_meta_boxes',     'txfx_plt_add_meta_box',      10, 2 );
-add_action( 'edit_post',         'txfx_plt_save_meta_box'            );
-?>
+add_action( 'save_post',         'txfx_plt_save_meta_box'            );
+add_action( 'init',              'txfx_plt_init'                     );
