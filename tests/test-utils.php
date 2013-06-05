820d71701071115fc19a38bdc8f73f99fe587224
Mark Jaquith <mark@jaquith.me>
2013-06-05 16:55:34 -0400

    Add tests for our util functions

---
+++ tests/test-utils.php
@@ -0,0 +1,14 @@
+<?php
+
+class CWS_PLT_Test_Utils extends CWS_PLT_TestCase {
+	function test_clean_url() {
+		foreach ( array(
+			'http://example.com/' => 'http://example.com/',
+			'  http://example.com/  ' => 'http://example.com/',
+			'www.example.com/' => 'http://www.example.com/',
+			' www.example.com/' => 'http://www.example.com/',
+		) as $in => $out ) {
+			$this->assertEquals( $out, $this->plugin()->clean_url( $in ) );
+		}
+	}
+}
