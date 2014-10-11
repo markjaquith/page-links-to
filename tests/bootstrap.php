7e2885c7a0453f0731275ce85ecb952f629adb25
Mark Jaquith <mark@jaquith.me>
2014-10-11 04:34:43 -0400

    Attempt to fix Travis

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,14 @@
 <?php
 
-require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
+$_tests_dir = getenv('WP_TESTS_DIR');
+if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+
+require_once $_tests_dir . '/includes/functions.php';
 
 function _manually_load_plugin() {
 	require dirname( __FILE__ ) . '/../page-links-to.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
-
-require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
 
 class CWS_PLT_TestCase extends WP_UnitTestCase {
 	function plugin() {
@@ -22,3 +23,5 @@ class CWS_PLT_TestCase extends WP_UnitTestCase {
 		unset( $_POST[$key], $_REQUEST[$key] );
 	}
 }
+
+require $_tests_dir . '/includes/bootstrap.php';
