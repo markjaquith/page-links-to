db669af6072d970fa8ff93b943842d950c51eeb0
Mark Jaquith <mark@coveredwebservices.com>
2019-08-21 15:53:21 -0400

    Update Travis and unit tests from WP-CLI scaffolding

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,33 @@
 <?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package Page_Links_To
+ */
 
-$_tests_dir = getenv('WP_TESTS_DIR');
-if ( !$_tests_dir ) $_tests_dir = '/tmp/wordpress-tests-lib';
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
 
+if ( ! $_tests_dir ) {
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
+}
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // WPCS: XSS ok.
+	exit( 1 );
+}
+
+// Give access to tests_add_filter() function.
 require_once $_tests_dir . '/includes/functions.php';
 
+/**
+ * Manually load the plugin being tested.
+ */
 function _manually_load_plugin() {
-	require dirname( __FILE__ ) . '/../page-links-to.php';
+	require dirname( dirname( __FILE__ ) ) . '/page-links-to.php';
 }
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
+// Start up the WP testing environment.
 require $_tests_dir . '/includes/bootstrap.php';
 
 class CWS_PLT_TestCase extends WP_UnitTestCase {
