94ac94c1e80de08bd3a880507fe0c24213485043
Mark Jaquith <mark@coveredwebservices.com>
2020-02-28 12:47:13 -0500

    Add an Internet Explorer polyfill for Element.closest()
    fixes #101

--- src/new-tab.js
+++ src/new-tab.js
@@ -1,4 +1,11 @@
+import elementClosest from 'element-closest';
 import { handleClicks, handleLoad } from './lib/newTab';
 
+// Add Element.closest() polyfill for Internet Explorer.
+elementClosest(window);
+
+// Listen to clicks for links that should be opened in a new tab.
 handleClicks();
+
+// Manipulate new-tab links that exist on load.
 handleLoad();
